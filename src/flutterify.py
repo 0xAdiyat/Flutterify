fd1a0157406dd583c15462a4aac8fd82021ab86f
0xadiyat <0xAdiyat@gmail.com>
2023-09-17 14:27:20 +0600

    Added Error Handling and Exit Options
    In this commit, I made the following changes to the script:
    
    1. Added error handling to gracefully handle cases where the user selects architecture options that are under construction ('MVVC' or 'MVC'). The script now informs the user and exits if they choose one of these options.
    
    2. Added explicit line breaks and spaces for user prompts to enhance readability.
    
    3. Added the ability for the user to cancel the process at the opening options step. If the user cancels, the script now exits gracefully.
    
    These changes improve the user experience and provide better feedback in various scenarios. The script now handles errors and exit options more gracefully.

--- src/flutterify.py
+++ src/flutterify.py
@@ -1,5 +1,11 @@
 import os
 import subprocess
+import sys
+import warnings
+
+import inquirer
+
+warnings.filterwarnings("ignore", category=UserWarning, module="blessed.terminal")
 
 
 class CodeSnippets:
@@ -112,20 +118,11 @@ def create_dart_file(directory, file_name, content=""):
         file.write(content)
 
 
-def open_in_android_studio(project_directory):
+def open_in_editor(editor, project_directory):
     try:
-        subprocess.run(["studio", project_directory])
+        subprocess.run([editor, project_directory])
     except FileNotFoundError:
-        print("Android Studio not found. Please make sure it is installed and in your system PATH.")
-        suggestion = "export PATH=$PATH:/Applications/Android\\ Studio.app/Contents/bin"
-        print(f"You can add Android Studio to your PATH by running the following command:\n{suggestion}")
-
-
-def open_in_vscode(project_directory):
-    try:
-        subprocess.run(["code", project_directory])
-    except FileNotFoundError:
-        print("Visual Studio Code not found. Please make sure it is installed and in your system PATH.")
+        print(f"{editor.capitalize()} not found. Please make sure it is installed and in your system PATH.")
 
 
 def clear_screen():
@@ -138,12 +135,41 @@ def insert_code_into_file(file_path, code_to_insert):
         file.write(code_to_insert)
 
 
+architecture_choices = [
+    inquirer.List('architecture',
+                  message="Choose an architecture:",
+                  choices=[
+                      ('Flutterify Special', '1'),
+                      ('MVVC (Under Construction)', '2'),
+                      ('MVC (Under Construction)', '3')
+                  ],
+                  default='1'
+                  )
+]
+
+opening_options = [
+    inquirer.List('open_option',
+                  message="How would you like to proceed?",
+                  choices=[
+                      ('Open in Android Studio', 'studio'),
+                      ('Open in Visual Studio Code', 'code'),
+                      ('Open the project directory', 'directory'),
+                      ('Cancel', 'cancel')
+                  ],
+                  default='code'
+                  )
+]
+
+
 def main():
     code_snippets = CodeSnippets()
 
     while True:
         project_name = input("Enter Flutter project name: ")
+        print("\n")
         package_name = input("Enter package name (or press Enter to use com.anon007 as default): ").strip()
+        print("\n")
+
         if not package_name:
             package_name = "com.anon007"
 
@@ -165,92 +191,88 @@ def main():
     os.chdir(studio_projects_directory)
 
     # Architecture selection
-    while True:
-        architecture_choice = input(
-            "Choose an architecture (Flutterify Special (1), MVVC (2), MVC (3)): ").strip()
-        if architecture_choice == '1' or architecture_choice == '':
+    architecture_choice = inquirer.prompt(architecture_choices)['architecture']
 
-            # Create the Flutter project with the specified package name
-            os.system(f'flutter create --org {package_name} {formatted_project_name}')
+    if architecture_choice == '1' or architecture_choice == '':
+        # Create the Flutter project with the specified package name
+        os.system(f'flutter create --org {package_name} {formatted_project_name}')
 
-            # Specify the directories to create within the project
-            directories_to_create = [
-                os.path.join(project_directory, "assets"),
-                os.path.join(project_directory, "assets", "icons"),
-                os.path.join(project_directory, "assets", "images"),
-                os.path.join(project_directory, "assets", "fonts"),
-                os.path.join(project_directory, 'lib', 'widgets'),
-                os.path.join(project_directory, 'lib', 'config'),
-                os.path.join(project_directory, 'lib', 'views'),
-                os.path.join(project_directory, 'lib', 'view_models'),
-                os.path.join(project_directory, 'lib', 'models'),
-                os.path.join(project_directory, 'lib', 'repository'),
-                os.path.join(project_directory, 'lib', 'data', 'network'),
-                os.path.join(project_directory, 'lib', 'data', 'response'),
-                os.path.join(project_directory, 'lib', 'config', 'constants'),
-                os.path.join(project_directory, 'lib', 'config', 'res', 'assets'),
-                os.path.join(project_directory, 'lib', 'config', 'res', 'routes'),
-                os.path.join(project_directory, 'lib', 'config', 'res', 'strings'),
-                os.path.join(project_directory, 'lib', 'config', 'themes')
-            ]
-            create_directories(project_directory, directories_to_create)
+        # Specify the directories to create within the project
+        directories_to_create = [
+            os.path.join(project_directory, "assets"),
+            os.path.join(project_directory, "assets", "icons"),
+            os.path.join(project_directory, "assets", "images"),
+            os.path.join(project_directory, "assets", "fonts"),
+            os.path.join(project_directory, 'lib', 'widgets'),
+            os.path.join(project_directory, 'lib', 'config'),
+            os.path.join(project_directory, 'lib', 'views'),
+            os.path.join(project_directory, 'lib', 'view_models'),
+            os.path.join(project_directory, 'lib', 'models'),
+            os.path.join(project_directory, 'lib', 'repository'),
+            os.path.join(project_directory, 'lib', 'data', 'network'),
+            os.path.join(project_directory, 'lib', 'data', 'response'),
+            os.path.join(project_directory, 'lib', 'config', 'constants'),
+            os.path.join(project_directory, 'lib', 'config', 'res', 'assets'),
+            os.path.join(project_directory, 'lib', 'config', 'res', 'routes'),
+            os.path.join(project_directory, 'lib', 'config', 'res', 'strings'),
+            os.path.join(project_directory, 'lib', 'config', 'themes')
+        ]
+        create_directories(project_directory, directories_to_create)
 
-            # Create Dart files in specific directories
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'constants'), 'app_const.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'constants'), 'size_config.dart')
+        # Create Dart files in specific directories
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'constants'), 'app_const.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'constants'), 'size_config.dart')
 
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'assets'), 'app_icons.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'assets'), 'app_images.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'assets'), 'app_icons.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'assets'), 'app_images.dart')
 
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'routes'), 'app_routes.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'routes'), 'route_paths.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'routes'), 'routes.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'routes'), 'app_routes.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'routes'), 'route_paths.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'routes'), 'routes.dart')
 
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'strings'), 'app_strings.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'res', 'strings'), 'app_strings.dart')
 
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'themes'), 'app_colors.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'config', 'themes'), 'app_themes.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'config'), 'config.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'themes'), 'app_colors.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config', 'themes'), 'app_themes.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'config'), 'config.dart')
 
-            create_dart_file(os.path.join(project_directory, 'lib'), 'app.dart')
+        create_dart_file(os.path.join(project_directory, 'lib'), 'app.dart')
 
-            create_dart_file(os.path.join(project_directory, 'lib', 'views'), 'views.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'view_models'), 'view_models.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'models'), 'models.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'widgets'), 'widgets.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'views'), 'views.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'view_models'), 'view_models.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'models'), 'models.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'widgets'), 'widgets.dart')
 
-            create_dart_file(os.path.join(project_directory, 'lib', 'views'), 'home_view.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'view_models'), 'home_viewmodel.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'views'), 'home_view.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'view_models'), 'home_viewmodel.dart')
 
-            create_dart_file(os.path.join(project_directory, 'lib', 'data'), 'app_exceptions.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'data', 'network'), 'base_api_services.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'data', 'network'), 'network_api_services.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'data', 'response'), 'api_response.dart')
-            create_dart_file(os.path.join(project_directory, 'lib', 'data', 'response'), 'status.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'data'), 'app_exceptions.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'data', 'network'), 'base_api_services.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'data', 'network'), 'network_api_services.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'data', 'response'), 'api_response.dart')
+        create_dart_file(os.path.join(project_directory, 'lib', 'data', 'response'), 'status.dart')
 
-            insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'constants', 'size_config.dart'),
-                                  code_snippets.size_config_code)
-            insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'config.dart'),
-                                  code_snippets.config_code)
-            insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'res', 'assets', 'app_icons.dart'),
-                                  code_snippets.app_icons_code)
-            insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'res', 'assets', 'app_images.dart'),
-                                  code_snippets.app_images_code)
-            insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'res', 'routes', 'route_paths.dart'),
-                                  code_snippets.route_paths)
-            insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'themes', 'app_themes.dart'),
-                                  code_snippets.app_themes)
+        insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'constants', 'size_config.dart'),
+                              code_snippets.size_config_code)
+        insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'config.dart'),
+                              code_snippets.config_code)
+        insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'res', 'assets', 'app_icons.dart'),
+                              code_snippets.app_icons_code)
+        insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'res', 'assets', 'app_images.dart'),
+                              code_snippets.app_images_code)
+        insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'res', 'routes', 'route_paths.dart'),
+                              code_snippets.route_paths)
+        insert_code_into_file(os.path.join(project_directory, 'lib', 'config', 'themes', 'app_themes.dart'),
+                              code_snippets.app_themes)
 
-            break
-        elif architecture_choice == '2':
-            print("MVVC architecture is under construction. Please wait for an update.")
-            break
-        elif architecture_choice == '3':
-            print("MVC architecture is under construction. Please wait for an update.")
-            break
-        else:
-            print("Invalid choice. Please enter '1' for Flutterify Special, '2' for MVVC, '3' for MVC, or press Enter "
-                  "for Flutterify Special (default).")
+
+    elif architecture_choice == '2':
+        print("MVVC architecture is under construction. Please wait for an update.")
+        sys.exit(0)
+
+    elif architecture_choice == '3':
+        print("MVC architecture is under construction. Please wait for an update.")
+        sys.exit(0)
 
     # Change into the project directory
     os.chdir(os.path.join(project_directory, 'lib'))
@@ -259,25 +281,19 @@ def main():
 
     print(f'Flutter project "{formatted_project_name}" created.')
 
-    while True:
-        choice = input(
-            "Do you want to open the project in Android Studio (A), Visual Studio Code (V), open the project "
-            "directory (D), or cancel (C)? ").strip().lower()
-        if choice == 'a':
-            open_in_android_studio(project_directory)
-            break
-        elif choice == 'v' or choice == '':
-            open_in_vscode(project_directory)
-            break
-        elif choice == 'd':
-            # Open the project directory
-            subprocess.run(["open", project_directory])
-        elif choice == 'c':
-            print("Process canceled. The project was created but not opened.")
-            break
-        else:
-            print("Invalid choice. Please enter 'A' for Android Studio, 'V' for Visual Studio Code, 'D' to open the "
-                  "project directory, or 'C' to cancel.")
+    # Prompt for opening options using inquirer
+    opening_option = inquirer.prompt(opening_options)['open_option']
+
+    if opening_option == 'studio':
+        open_in_editor("studio", project_directory)
+
+    elif opening_option == 'code':
+        open_in_editor("code", project_directory)
+    elif opening_option == 'directory':
+        subprocess.run(["open", project_directory])
+    elif opening_option == 'cancel':
+        print("Process canceled. The project was created but not opened.")
+        sys.exit(0)
 
 
 if __name__ == '__main__':
